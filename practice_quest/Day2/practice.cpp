a302a7e5a5fe56c04600b067db52d73e3559a134
skan00 <anvisking@gmail.com>
2021-06-26 11:31:46 +0545

    practice

---
+++ practice_quest/Day2/practice.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class myclass
+{
+    private:
+    int x;
+
+    public:
+    void setx(int i )
+    {
+        x=i;
+    }
+    int getx()
+    {
+        return x;
+    }
+};
+int main()
+{
+    myclass classes[4];
+    
+    int i;
+    for (i=0; i<4; i++)
+    {
+        classes[i].setx(i);
+    }
+    for(i=0;i<4;i++)
+    {
+        cout<<"classes["<<i<<"]: "<<classes[i].getx()<<endl;
+    }
+
+}
